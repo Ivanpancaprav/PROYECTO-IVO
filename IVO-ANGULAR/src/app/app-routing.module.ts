5ff274d239cf8511f6c829402041390a53d5f7bf
Caki97 <thebigcaki@gmail.com>
2023-01-18 08:56:40 +0100

    alba

--- IVO-ANGULAR/src/app/app-routing.module.ts
+++ IVO-ANGULAR/src/app/app-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PerfilComponent } from './usuarios/perfil/perfil.component';
 import { CitasComponent } from './usuarios/citas/citas.component';
-<<<<<<< HEAD
 import { PacientesComponent } from './usuarios/pacientes/pacientes.component';
-=======
 import { CitapreviaComponent } from './usuarios/citaprevia/citaprevia.component';
 
 const routes: Routes = [
@@ -15,11 +13,9 @@ const routes: Routes = [
   {
     path:'citas',
     component: CitasComponent
-<<<<<<< HEAD
   },{
     path:'pacientes',
     component: PacientesComponent
-=======
   },
   {
     path:'citaprevia',
