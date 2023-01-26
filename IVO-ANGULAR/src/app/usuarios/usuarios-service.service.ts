b2765668bd60409346340cf4310a3fa27250ba3f
Ipan <ivanpancaprav@gmail.com>
2023-01-26 13:46:57 +0100

    Intento de datatables

---
+++ IVO-ANGULAR/src/app/usuarios/usuarios-service.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import  {HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http'
+import { Observable } from 'rxjs';
+
+const HttpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':'application/json'
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosServiceService {
+  [x: string]: any;
+  public url: string;
+
+  constructor(private Http: HttpClient) {
+    this.url ="http://localhost/api/pacientes";
+   }
+
+   getPacientes(){
+      return this.Http.get(this.url);
+   }
+}
