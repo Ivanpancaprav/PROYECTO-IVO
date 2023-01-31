05043f301fd132634e5a16a21c009ec72efee472
Ipan <ivanpancaprav@gmail.com>
2023-01-31 11:16:24 +0100

    Fusion

---
+++ IVO-ANGULAR/src/app/models/paciente.model.ts
@@ -0,0 +1,10 @@
+export class Paciente {
+    dni?: any;
+    nombre?: string;
+    apellido1?: string;
+    email?: string;
+    sexo?: string;
+    fecha_nacimiento?: string;
+    n_seguridad_social?: BigInt;
+    n_historial_clinico?: BigInt;
+}
